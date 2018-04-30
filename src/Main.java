b734042d2a553b75560ac8b1a267522c4198663b
Eliotsehr <sadrin.eliot@live.Fr>
2018-05-01 00:04:04 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,25 @@
 
 
 public class Main {
-	
-	//Wesh le sang
-	
-	
+
 	static Interface interf;
-	
+
 	static Jeu risk;
- 
+
 	static boolean jeu = true;
-	
-	public static void main(String[] args) {	
+
+	public static void main(String[] args) {
 		risk = new Jeu();
-		
+
 		interf = new Interface(1080,720,0);
 		
 		interf.ecranMenu();
-		
+
 		while(jeu)
 		{
-			interf.cliqueCouche(interf.getCouche());	
+			interf.cliqueCouche(interf.getCouche());
 		}
-		
+
 		interf.coucheVictoire();
 	}
 }
